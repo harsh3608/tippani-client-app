8fcf1bdd300f40c57c7df606dbf83dd5320dccf7
harsh3608 <harsh3608@gmail.com>
2024-05-26 14:39:42 +0530

    added new components

---
+++ src/app/feed/feed-home/feed-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feed-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './feed-home.component.html',
+  styleUrl: './feed-home.component.css'
+})
+export class FeedHomeComponent {
+
+}
