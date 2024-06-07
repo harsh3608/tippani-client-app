8b93c2486d3ed6d32c93f3688aac4e3adcaa50fe
HarshPatel89 <harsh.b.patel@convergesolution.com>
2024-06-07 11:15:44 +0530

    added more code

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { FacebookLoginProvider, GoogleSigninButtonModule, SocialAuthService, SocialLoginModule, SocialUser } from '@abacritt/angularx-social-login';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule,
+    SocialLoginModule,
+    GoogleSigninButtonModule,
+    ButtonModule,
+    InputTextModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  user!: SocialUser;
+  loggedIn: boolean = false;
+
+  constructor(
+    private authService: SocialAuthService
+  ) { }
+
+  ngOnInit(): void {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+      console.log(user);
+    });
+  }
+
+  signInWithFB(): void { //Facebook Login
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): any { //for logging out
+    this.authService.signOut();
+    console.log('logged out');
+  }
 
 }
